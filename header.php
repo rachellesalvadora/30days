4d03fb98220381afd667a64f3ceceb9807c68062
Rachelle Salvadora <rachellesalvadora@gmail.com>
2015-02-24 11:04:52 +1100

    30 days first commit day 1 added

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+
+	<title>30 DAY CHALLENGE</title>
+
+	<!--[if IE]>
+		<script type="text/javascript">
+			 var console = { log: function() {} };
+		</script>
+	<![endif]-->
+
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+<div class="sticky-link-home">
+
+<h3 class="sticky-home">HOME</h3>
+
+<div class="sticky-links">
+<ul class="sticky-ul">
+
+<li class="sticky-li">Website
+</li>
+
+<li class="sticky-li"> Blog
+</li>
+
+<li class="sticky-li"> Github
+</li>
+
+</ul>
+</div>
+
+</div>
+
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+	include "header.php";
+?>
+
+
+<div class="body-title">
+<h1 class="banner">
+30 DAY CHALLENGE
+</h1>
+<p class="banner-p">
+A challenge that I'm doing with friends. Each of us will do 1 website per day.
+</p>
+
+</div>
+
+
+<div class="list">
+<ul class="day-ul">
+
+<li class="day-li">
+<a class="day-a" href="index.php">
+	DAY 1: "Home Page"
+</a>
+</li>
+
+<li class="day-li">
+<a class="day-a" href="">DAY 2
+</a>
+</li>
+
+<li class="day-li">
+<a class="day-a" href="">
+</a>
+</li>
+
+<li class="day-li">
+<a class="day-a" href="">
+</a>
+</li>
+
+<li class="day-li">
+<a class="" href="">
+</a>
+</li>
+
+<li class="day-li">
+<a class="" href="">
+</a>
+</li>
+
+
+
+
+</ul>
+
+</div>
